afc4adc45a194ccf6a1f2c62c26a297ee36c3643
Jérôme Pott <jeromepott@hotmail.com>
2019-08-11 19:21:55 +0200

    Add E2E tests for examples 1-3 (#1)
    * Add Cypress
    
    * Add DS_store to git
    
    * Test examples 1 to 3
    
    * Fix linting
    
    * Check for error status 405

--- components/Example3.vue
+++ components/Example3.vue
@@ -44,10 +44,10 @@ export default {
   <ElForm ref="form" :model="form" inline label-width="auto" label-position="left" @submit.native.prevent="helloNamePost(form.name)">
     <h2>3. Hello, {name} (POST version)</h2>
     <ElFormItem label="Name">
-      <ElInput v-model="form.name" placeholder="Your name" />
+      <ElInput v-model="form.name" placeholder="Your name" data-cy="input-hello-name-post" />
     </ElFormItem>
-    <ElButton type="primary" @click="helloNamePost(form.name)">👋 Say hello</ElButton>
-    <ElButton type="danger" @click="helloNamePostError(form.name)">.$get() Error</ElButton>
+    <ElButton type="primary" data-cy="btn-hello-name-post" @click="helloNamePost(form.name)">👋 Say hello</ElButton>
+    <ElButton type="danger" data-cy="btn-hello-name-post-error" @click="helloNamePostError(form.name)">.$get() Error</ElButton>
     <p>Response: {{ response }}</p>
     <p v-if="error" style="color:red;"><strong>Error {{ error.status }}</strong><br>{{ error.data }}</p>
   </ElForm>
